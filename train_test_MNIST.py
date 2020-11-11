8b25cf8b1b6481731b1ed466a20c361781a3390b
deanone <athanasios.salamanis@gmail.com>
2020-11-11 11:01:15 +0200

    Put all the logic into a main() function

--- train_test_MNIST.py
+++ train_test_MNIST.py
@@ -1,32 +1,40 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 from keras.datasets import mnist
 from keras import models
 from keras import layers
 from keras.utils import to_categorical
 
-# Load MNIST data
-(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
-# Change shape and type of input samples
-train_images = train_images.reshape((60000, 28 * 28))
-train_images = train_images.astype('float32') / 255
+def main():
 
-test_images = test_images.reshape((10000, 28 * 28))
-test_images = test_images.astype('float32') / 255
+	# Load MNIST data
+	(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
-# Categorical encoding of labels
-train_labels = to_categorical(train_labels)
-test_labels = to_categorical(test_labels)
+	# Change shape and type of input samples
+	train_images = train_images.reshape((60000, 28 * 28))
+	train_images = train_images.astype('float32') / 255
 
-# Build network
-network = models.Sequential()
-network.add(layers.Dense(512, activation='relu', input_shape=(28 * 28, )))
-network.add(layers.Dense(10, activation='softmax'))
-network.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+	test_images = test_images.reshape((10000, 28 * 28))
+	test_images = test_images.astype('float32') / 255
 
-# Train network
-network.fit(train_images, train_labels, epochs=5, batch_size=128)
+	# Categorical encoding of labels
+	train_labels = to_categorical(train_labels)
+	test_labels = to_categorical(test_labels)
 
-# Evaluate network
-test_loss, test_acc = network.evaluate(test_images, test_labels)
-print('test_acc:', test_acc)
+	# Build network
+	network = models.Sequential()
+	network.add(layers.Dense(512, activation='relu', input_shape=(28 * 28, )))
+	network.add(layers.Dense(10, activation='softmax'))
+	network.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 
+	# Train network
+	network.fit(train_images, train_labels, epochs=10, batch_size=32)
+
+	# Evaluate network
+	test_loss, test_acc = network.evaluate(test_images, test_labels)
+	print('test_acc:', test_acc)
+
+
+if __name__ == '__main__':
+	main()
